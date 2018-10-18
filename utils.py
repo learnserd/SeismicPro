9b1c846401e052ead66612e25fc39b2bc6418f0a
illarionovEA <e.illarionov@analysiscenter.org>
2018-10-18 19:12:21 +0300

    one line model

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ class IndexTracker(object):
         self.ax = ax
         self.X = X
         self.step = step
-        self.slice_names = (np.arange(X.shape[-1], dtype="int") if slice_names is None
-                            else slice_names)
+        self.slice_names = (np.arange(X.shape[-1], dtype="int")
+                            if slice_names is None else slice_names)
         self.cmap = ("gray" if cmap is None else cmap)
-        
+
         rows, cols, self.slices = X.shape
         self.ind = self.slices//2
 
@@ -38,22 +38,25 @@ def update_counters(batch, counters, iter_config):
     stride = iter_config["strides"]
     slice_axis = iter_config["slice_axis"]
     slice_indices = iter_config["slice_indices"]
-    
+
     if counters is None:
         return [0, 0]
+
     slice_shape = np.delete(traces_shape, slice_axis)
     grid_size = np.prod(np.ceil(slice_shape / stride)).astype(int)
     if (counters[1] + 1) * batch_size < grid_size:
         return [counters[0], counters[1] + 1]
-    
+
     if counters[0] == len(slice_indices) - 1:
         raise StopIteration
-   
+
     return [counters[0] + 1, 0]
+
 
 def get_start_index(batch, counters, iter_config):
     _ = batch
     return counters[1] * iter_config["batch_size"]
+
 
 def get_slice_index(batch, counters, iter_config):
     _ = batch
