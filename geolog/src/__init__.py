41f5bda2f4d4ad37201b9bff29ace483a5ab1328
illarionovEA <e.illarionov@analysiscenter.ru>
2019-02-20 14:49:09 +0000

    fx init

--- geolog/src/__init__.py
+++ geolog/src/__init__.py
@@ -1,4 +1,5 @@
 """Init file"""
 from .seismic_batch import SeismicBatch
-from .field_index import FieldIndex, TraceIndex, BinsIndex, SegyFilesIndex, DataFrameIndex
+from .field_index import (FieldIndex, TraceIndex, BinsIndex, SegyFilesIndex,
+                          DataFrameIndex, IlineIndex, XlineIndex)
 from .utils import get_file_by_index, Layouts
