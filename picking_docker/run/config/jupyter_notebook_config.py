ecf0c6f9a51a720de1e9e8148dd0ec7a53255325
nikita <nikita_007_94@mail.ru>
2019-06-05 09:52:00 +0300

    docker container with Seismicpro inside

--- picking_docker/run/config/jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
-c.NotebookApp.token = u'password'
-c.NotebookApp.allow_root = True
--- picking_docker/utils/create_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#mkdir ./notebooks
-#rm -rf ./run/notebooks
-#ln -s /picking ./run/notebooks
-
-sudo docker pull analysiscenter1/ds-py3:cpu  #
