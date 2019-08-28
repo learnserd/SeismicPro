f4529bf2caf2bc2342157c6a434857af07be7939
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-08-28 18:55:47 +0300

    added multiple reindexings in different directions

--- test_switch_indices.py
+++ test_switch_indices.py
@@ -30,8 +30,13 @@ class CheckIndicesMixin:
         return self
 
     @action
-    def assert_index_type(self, index_type):
+    def assert_index_type(self, index_type, deb=None):
         """ insure index is of given type """
+        if deb is not None:
+            if len(deb & set(self.indices)):
+                print(deb & set(self.indices))
+
+            deb.update(set(self.indices))
         if not isinstance(self.index, index_type):
             raise ValueError("Index must be {}, not {}".format(index_type, type(self.index)))
         return self
@@ -73,8 +78,11 @@ class ReindexerBatchMixin:
         new_fields, new_data = self.reindex_stats.get_ready_idx(self.components, self_no)
 
         new_index_type = self.reindex_stats.new_index_type(self_no)
-        new_batch = type(self)(new_index_type(self.index).create_subset(new_fields))
+
+        new_index_big = new_index_type(self.dataset.index)
+        new_batch = type(self)(new_index_big.create_subset(new_fields))
         new_batch.add_components(self.components, init=new_data)
+        new_batch.dataset = self.dataset
 
         return new_batch
 
@@ -94,26 +102,34 @@ class ChangeIndicesDataSet(Dataset):
         }
 
         def __init__(self, index, old_index_type, new_index_type):
-            old_col = self.df_columns[old_index_type]
+            self.old_col = old_col = self.df_columns[old_index_type]
             self.new_col = new_col = self.df_columns[new_index_type]
 
-            self.imap = index.get_df()[[old_col, new_col]].droplevel(1, axis=1).set_index(old_col)
+            df = index.get_df()
+            self.imap = df[[old_col, new_col]].droplevel(1, axis=1) #.set_index(old_col)
+
+            # hack
+            if TraceIndex in (old_index_type, new_index_type):
+                self.imap['TRACE_SEQUENCE_FILE'] = self.imap['TRACE_SEQUENCE_FILE'] - 1
+
             self.new_fields = {}
             self.processed_fields = set()
             self.new_index_type = new_index_type
             self.old_index_type = old_index_type
 
         def add_item(self, old_idx, components, data):
+            old_imap_indices = self.imap[self.imap[self.old_col] == old_idx].index
+            new_items = self.imap.loc[old_imap_indices][self.new_col]
+            self.imap = self.imap.drop(old_imap_indices)
 
-            deb = self.imap.loc[old_idx + 1]
-            new_item = deb[self.new_col]
-            self.imap.drop(old_idx + 1, inplace=True)
+            need_split = len(new_items) > 1
 
-            self.new_fields.setdefault(new_item, dict.fromkeys(components, []))
-            for c, d in zip(components, data):
-                self.new_fields[new_item][c].append(d)
+            for i, new_item in enumerate(new_items):
+                self.new_fields.setdefault(new_item, dict.fromkeys(components, []))
+                for c, d in zip(components, data):
+                    self.new_fields[new_item][c].append(d[i].reshape(1, -1) if need_split else d)
 
-            self.processed_fields.add(new_item)
+                self.processed_fields.add(new_item)
 
         def get_ready_idx(self, components):
             """ get fields whose traces are all processed """
@@ -163,21 +179,35 @@ if __name__ == "__main__":
 
     trace_index = TraceIndex(name='raw', path=base_path, extra_headers=['ShotPoint', 'offset'])
 
+    orig = set(trace_index.indices)
+
     fi = FieldIndex(trace_index)
 
     ds = ChangeIndicesDataSet(trace_index, CheckIndicesBatch)
-    ds.initialize_reindex_stats([(TraceIndex, FieldIndex)])
+    ds.initialize_reindex_stats([(TraceIndex, FieldIndex), (FieldIndex, TraceIndex)])
 
     p1 = (Pipeline().load(components='raw', fmt='sgy'))
 
+    s1 = set()
+    s2 = set()
+    s3 = set()
+
     p2 = (p1
-          .assert_index_type(TraceIndex)
+          .assert_index_type(TraceIndex, s1)
           .assert_traces_shape()
           .reindex(0)
           .assert_fields_shape()
-          .assert_index_type(FieldIndex)
+          .assert_index_type(FieldIndex, s2)
+          .reindex(1)
+          .assert_index_type(TraceIndex, s3)
+          .assert_traces_shape()
           )
 
     p2 = p2 << ds
 
     p2.run(batch_size=800, n_epochs=1, bar=True)
+
+    print(len(s1), len(s2), len(s3))
+    print(s2)
+    print(orig - set(s1))
+    print(orig - set(s3))
