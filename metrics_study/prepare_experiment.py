bbbef8468d9397f45150793721c6ee4bd7ce8af8
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-09-26 14:28:47 +0300

    utils for preparing blind test - shuffle_folders + minor changes

--- metrics_study/prepare_experiment.py
+++ metrics_study/prepare_experiment.py
@@ -3,19 +3,28 @@
 
 import os
 import shutil
-# import random
+import random
 from functools import reduce
-from collections import namedtuple
+import csv
 
 import numpy as np
 
 from seismicpro.batchflow import Dataset, Pipeline
 from seismicpro.src import SeismicBatch, FieldIndex
 
-Parameters = namedtuple('Parameters', 'models_paths raw_path fields')
 
-base_path = '/home/antonina/winhome/datasets/multiple_lift_4_metrics_validation/QC/'
+class Parameters:
+    """ Input data for experiment """
+    def __init__(self, models_paths, raw_path, fields):
+        self.models_paths = models_paths
+        self.raw_path = raw_path
+        self.fields = fields
+        self.num_models = len(self.models_paths)
+        self.model_names = ['M_' + str(i) for i in range(self.num_models)]
+        self.num_fields = len(self.fields)
 
+
+base_path = '/datasets/multiple_lift_4_metrics_validation/QC/'
 test_params = Parameters(
     models_paths=[base_path + p for p in [
         '1_lift.sgy',
@@ -43,20 +52,17 @@ def split_folders(res_path, params):
     ...  +- i1.sgy
          +- ...
     """
-    num_models = len(params.models_paths)
-    model_names = ['M_' + str(i) for i in range(num_models)]
-
     if os.path.exists(res_path):
         shutil.rmtree(res_path)
 
     field_index = reduce(lambda x, y: x.merge(y),
                          (FieldIndex(name=name, path=path, extra_headers=['offset'])
-                          for name, path in zip(model_names, params.models_paths)))
+                          for name, path in zip(params.model_names, params.models_paths)))
     field_index = field_index.merge(FieldIndex(name='raw',
                                                path=params.raw_path, extra_headers=['offset']))
     field_index = field_index.create_subset(np.asarray(params.fields))
 
-    components = tuple(model_names + ['raw'])
+    components = tuple(params.model_names + ['raw'])
     p = (Pipeline(dataset=Dataset(field_index, SeismicBatch))
          .load(fmt='segy', components=components)
          .sort_traces(src=components, dst=components, sort_by='offset')
@@ -72,48 +78,64 @@ def split_folders(res_path, params):
 
     # print model paths and model aliases correspondence to file
     with open(os.path.join(res_path, 'description.txt'), 'w') as f:
-        for name, path in zip(model_names, params.models_paths):
+        for name, path in zip(params.model_names, params.models_paths):
             f.write('{},"{}"\n'.format(name, path))
 
 
+def shuffle_folders(path_to_fields, path_to_res, num_experts, params):
+    """
+    prepare files for each expert individually
+    model aliases are shuffled
+    """
+    if os.path.exists(path_to_res):
+        shutil.rmtree(path_to_res)
 
+    mapping = {}
+    for i in range(num_experts):
+        expert_name = 'Expert_{}'.format(i)
+        mapping[expert_name] = {}
 
-# def shuffle_folders(path_to_fields, path_to_res, num_experts, params):
-#     mapping = {}
-#     for i in range(num_experts):
-#         expert_name = 'Expert_{}'.format(i)
-#         mapping[expert_name] = {}
-#
-#         expert_path = path_to_res + expert_name
-#         os.makedirs(expert_path)
-#
-#         for field_no in params.fields:
-#             field_name = 'F_{}'.format(field_no)
-#             field_path_new = expert_path + field_name
-#             field_path_old = path_to_fields + field_name
-#             os.makedirs(field_path_new)
-#
-#             mapping[expert_name][field_name] = {}
-#
-#             num_models = len(params.models_paths)
-#             new_indices = random.sample(range(num_models), k=num_models)
-#             for old_idx, new_idx in enumerate(new_indices):
-#
-#                 model_name_old = 'M_{}_{}.sgy'.format(field_no, old_idx)
-#                 model_name_new = 'M_{}_{}.sgy'.format(field_no, new_idx)
-#                 model_path_old = field_path_old + model_name_old
-#                 model_path_new = field_path_new + model_name_new
-#
-#                 os.copy(model_path_old, model_path_new)
-#                 os.copy(params.raw_path, field_path_new + 'raw.sgy')
-#
-#                 mapping[expert_name][field_name][new_idx] = params.models_paths[old_idx]
-#
-#     # print mapping to file
-#
-#     pass
+        expert_path = os.path.join(path_to_res, expert_name)
+        os.makedirs(expert_path)
+
+        for fi in params.fields:
+            field_name = 'F_{}'.format(fi)
+            field_dir = os.path.join(expert_path, field_name)
+            os.makedirs(field_dir)
+            mapping[expert_name][field_name] = {}
+
+            new_indices = random.sample(range(params.num_models), k=params.num_models)
+            for old_idx, new_idx in enumerate(new_indices):
+                model_name = params.model_names[old_idx]
+                field_path_old = os.path.join(path_to_fields, model_name, '{}.sgy'.format(fi))
+                field_name_new = 'M_{}_{}.sgy'.format(fi, new_idx)
+                field_path_new = os.path.join(field_dir, field_name_new)
+
+                # debugging
+                # print(field_path_old)
+                # print("->")
+                # print(field_path_new)
+                # print()
+
+                shutil.copyfile(field_path_old, field_path_new)
+
+                mapping[expert_name][field_name][field_name_new] = params.models_paths[old_idx]
+
+    # print mapping to file
+    with open(os.path.join(path_to_res, 'description.csv'), 'w') as csv_file:
+        csvwriter = csv.writer(csv_file, quoting=csv.QUOTE_NONNUMERIC)
+        csvwriter.writerow(['expert_name', 'field_name', 'model_name', 'true_model'])
+        for expert_name in mapping:
+            for field_name in mapping[expert_name]:
+                for model_name in sorted(mapping[expert_name][field_name]):
+                    csvwriter.writerow([expert_name,
+                                        field_name,
+                                        model_name,
+                                        mapping[expert_name][field_name][model_name]
+                                        ])
 
 
 if __name__ == '__main__':
     test_res_path = "test_res"
     split_folders(test_res_path, test_params)
+    shuffle_folders(test_res_path, "experts", num_experts=3, params=test_params)
