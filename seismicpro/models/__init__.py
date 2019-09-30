f79b15180fe057c424534c0786a6692f8ca54f71
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-09-30 10:49:10 +0300

    UnetAttention3 (common encoder & skips)

--- seismicpro/models/__init__.py
+++ seismicpro/models/__init__.py
@@ -2,3 +2,4 @@
 from .hmm_model import *  # pylint: disable=wildcard-import
 from .unet_attention import *  # pylint: disable=wildcard-import
 from .metrics import *  # pylint: disable=wildcard-import
+from .unetatt_3 import UnetAttention3
