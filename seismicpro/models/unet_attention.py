bb05b08b2d652ac961b1d2a21c55147c8259263a
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-10-01 18:39:53 +0300

    UnetAttention4 ( common encoder & skips after concat)

--- seismicpro/models/unet_attention.py
+++ seismicpro/models/unet_attention.py
@@ -2,9 +2,157 @@
 import tensorflow as tf
 
 from ..batchflow.batchflow.models.tf import EncoderDecoder
-from ..batchflow.batchflow.models.tf.layers import conv_block
+from ..batchflow.batchflow.models.tf.layers import conv_block, combine
+from ..batchflow.batchflow.models.utils import unpack_args
 
-class UnetAtt(EncoderDecoder):
+
+class EncoderDecoderWithBranch(EncoderDecoder):
+    """ EncoderDecoderWithBranch """
+    @classmethod
+    def body(cls, inputs, name='body', **kwargs):
+        """ Create encoder, embedding and decoder. """
+        kwargs = cls.fill_params('body', **kwargs)
+        encoder = kwargs.pop('encoder')
+        embeddings = kwargs.get('embedding')
+        decoder = kwargs.pop('decoder')
+
+        with tf.variable_scope(name):
+            # Encoder: transition down
+            if encoder is not None:
+                encoder_args = {**kwargs, **encoder}
+                encoder_outputs = cls.encoder(inputs, name='encoder', **encoder_args)
+            else:
+                encoder_outputs = [inputs]
+            x = encoder_outputs[-1]
+
+            # Bottleneck: working with compressed representation via multiple steps of processing
+            if embeddings is not None:
+                embeddings = embeddings if isinstance(embeddings, (tuple, list)) else [embeddings]
+
+                for i, embedding in enumerate(embeddings):
+                    embedding_args = {**kwargs, **embedding}
+                    x = cls.embedding(x, name='embedding-'+str(i), **embedding_args)
+            encoder_outputs[-1] = x
+
+            # Decoder: transition up
+            if decoder is not None:
+                decoder_args = {**kwargs, **decoder}
+                x = cls.decoder(encoder_outputs[::-1], name='decoder', **decoder_args)
+        return x
+
+    @classmethod
+    def decoder(cls, inputs, name='decoder', **kwargs):
+        steps = kwargs.pop('num_stages') or len(inputs)-1
+        factor = kwargs.pop('factor') or [2]*steps
+        skip, order, upsample, block_args = cls.pop(['skip', 'order', 'upsample', 'blocks'], kwargs)
+        order = ''.join([item[0] for item in order])
+        base_block = block_args.get('base')
+
+        if isinstance(factor, int):
+            factor = int(factor ** (1/steps))
+            factor = [factor] * steps
+        elif not isinstance(factor, list):
+            raise TypeError('factor should be int or list of int, but %s was given' % type(factor))
+
+        with tf.variable_scope(name):
+            x = inputs[0]
+
+            for i in range(steps):
+                with tf.variable_scope('decoder-'+str(i)):
+                    # Skip some of the steps
+                    if factor[i] == 1:
+                        continue
+
+                    # Make all the args
+                    args = {**kwargs, **block_args, **unpack_args(block_args, i, steps)}
+                    upsample_args = {'factor': factor[i],
+                                     **kwargs, **upsample, **unpack_args(upsample, i, steps)}
+
+                    combine_op = args.get('combine_op')
+                    combine_args = {'op': combine_op if isinstance(combine_op, str) else '',
+                                    'data_format': args.get('data_format'),
+                                    **(combine_op if isinstance(combine_op, dict) else {}),
+                                    **(skip if isinstance(skip, dict) else {})}
+
+                    for letter in order:
+                        if letter == 'b':
+                            x = base_block(x, name='block', **args)
+                        elif letter in ['u']:
+                            if upsample.get('layout') is not None:
+                                x = cls.upsample(x, name='upsample', **upsample_args)
+                        elif letter == 'c':
+                            # Combine result with the stored encoding of the ~same shape
+                            if (skip or isinstance(skip, dict)) and (i < steps):
+                                x = cls.crop(x, inputs[i+1], data_format=kwargs.get('data_format'))
+                                x = combine([x, inputs[i+1]], **combine_args)
+                        else:
+                            raise ValueError('Unknown letter in order {}, use one of ("b", "u", "c")'.format(letter))
+
+        return x
+
+    @classmethod
+    def branch(cls, inputs, name='branch', **kwargs):
+        """ Branch, consisting of blocks """
+        steps = kwargs.pop('num_stages') or len(inputs) - 1
+
+        order, downsample, block_args = cls.pop(['order', 'downsample', 'blocks'], kwargs, default=None)
+        skip, upsample = cls.pop(['skip', 'upsample'], kwargs, default=None)
+        order = ''.join([item[0] for item in order])
+
+        x = inputs[0]
+        branch_outputs = []
+
+        with tf.variable_scope(name):
+            for i in range(steps):
+                with tf.variable_scope('block-' + str(i)):
+                    # Make all the args
+                    args = {**kwargs, **block_args, **unpack_args(block_args, i, steps)}
+
+                    for letter in order:
+                        if letter == 'b':
+
+                            base_block = block_args.get('base')
+                            x = base_block(x, name='block', **args)
+                        elif letter == 's':
+                            branch_outputs.append(x)
+                        elif letter in ['d', 'p']:
+                            downsample_args = {**kwargs, **downsample, **unpack_args(downsample, i, steps)}
+                            if downsample.get('layout') is not None:
+                                x = conv_block(x, name='downsample', **downsample_args)
+                        elif letter in ['u']:
+                            factor = kwargs.get('factor')
+                            if factor is None:
+                                factor = 2
+                            elif isinstance(factor, int):
+                                factor = int(factor ** (1 / steps))
+                            elif isinstance(factor, list):
+                                factor = factor[i]
+                            else:
+                                raise TypeError('factor should be int or list of int, but %s was given' % type(factor))
+
+                            upsample_args = {**kwargs, 'factor': factor,
+                                             **upsample, **unpack_args(upsample, i, steps)}
+                            if upsample.get('layout') is not None:
+                                x = cls.upsample(x, name='upsample', **upsample_args)
+                        elif letter == 'c':
+                            combine_op = args.get('combine_op')
+                            combine_args = {'op': combine_op if isinstance(combine_op, str) else '',
+                                            'data_format': args.get('data_format'),
+                                            **(combine_op if isinstance(combine_op, dict) else {}),
+                                            **(skip if isinstance(skip, dict) else {})}
+                            # Combine result with the stored encoding of the ~same shape
+                            if (skip or isinstance(skip, dict)) and (i < steps):
+                                x = cls.crop(x, inputs[i + 1], data_format=kwargs.get('data_format'))
+                                x = combine([x, inputs[i + 1]], **combine_args)
+                        else:
+                            raise ValueError('Unknown letter in order {}, use one of "b", "d", "p", "s"'
+                                             .format(letter))
+
+        branch_outputs.append(x)
+        return branch_outputs
+
+
+class UnetAtt(EncoderDecoderWithBranch):
     """Class for Unet Attention model."""
 
     @classmethod
@@ -77,6 +225,7 @@ class UnetAtt(EncoderDecoder):
         self.store_to_attr("out_lift", out_lift)
 
         return tf.stack([out_lift, attention_sigmoid], axis=0)
+
 
 def attention_loss(targets, predictions, balance, **kwargs):
     """Loss function for Unet Attention model
--- seismicpro/models/unetatt_3.py
+++ seismicpro/models/unetatt_3.py
@@ -51,16 +51,84 @@ class UnetAttention3(UnetAtt):
                 for i, embedding in enumerate(embeddings):
                     embedding_args = {**kwargs, **embedding}
                     x = cls.embedding(x, name='embedding-'+str(i), **embedding_args)
-            encoder_outputs.append(x)
+            encoder_outputs[-1] = x
 
             # Decoder: transition up
             if decoder is not None:
                 decoder_args = {**kwargs, **decoder}
-                main = cls.decoder(encoder_outputs, name='decoder', **decoder_args)
+                main = cls.decoder(encoder_outputs[::-1], name='decoder', **decoder_args)
 
             # Attention: transition up
             if attention is not None:
                 attention_args = {**kwargs, **attention}
-                att = cls.decoder(encoder_outputs, name='attention', **attention_args)
+                att = cls.decoder(encoder_outputs[::-1], name='attention', **attention_args)
 
         return main, att, raw, offset
+
+
+class UnetAttention4(UnetAtt):
+    """ Unet Attention iss3: common encoder and skips """
+
+    @classmethod
+    def default_config(cls):
+        config = EncoderDecoder.default_config()
+
+        config['body/encoder/num_stages'] = 4
+        config['body/encoder/order'] = ['block', 'skip', 'downsampling']
+        config['body/encoder/blocks'] += dict(layout='cna cna', kernel_size=3,
+                                              filters=[64, 128, 256, 512])
+
+        config['body/embedding'] += dict(layout='cna cna', kernel_size=3, filters=1024)
+
+        config['body/decoder/order'] = ['upsampling', 'combine', 'block']
+        config['body/decoder/blocks'] += dict(layout='cna cna', kernel_size=3,
+                                              filters=[512, 256, 128, 64])
+
+        config['body/attention'] = config['body/decoder']
+
+        config['body/attention/order'] = ['upsampling', 'combine', 'skip', 'block']
+        config['body/attention/blocks'] += dict(layout='cna cna', kernel_size=3,
+                                                filters=[512, 256, 128, 64])
+
+        return config
+
+    @classmethod
+    def body(cls, inputs, name='body', **kwargs):
+        """ Create encoder, embedding and 2 decoder branches: main and attention """
+        kwargs = cls.fill_params('body', **kwargs)
+        encoder = kwargs.pop('encoder')
+        embeddings = kwargs.get('embedding')
+        decoder = kwargs.pop('decoder')
+        attention = kwargs.pop('attention')
+
+        raw, offset = inputs
+
+        with tf.variable_scope(name):
+            # Encoder: transition down
+            if encoder is not None:
+                encoder_args = {**kwargs, **encoder}
+                encoder_outputs = cls.branch([raw], name='encoder', **encoder_args)
+            else:
+                encoder_outputs = [raw]
+            x = encoder_outputs[-1]
+
+            # Bottleneck: working with compressed representation via multiple steps of processing
+            if embeddings is not None:
+                embeddings = embeddings if isinstance(embeddings, (tuple, list)) else [embeddings]
+
+                for i, embedding in enumerate(embeddings):
+                    embedding_args = {**kwargs, **embedding}
+                    emb = cls.embedding(x, name='embedding-'+str(i), **embedding_args)
+            encoder_outputs[-1] = emb
+
+            # Attention: transition up
+            if attention is not None:
+                attention_args = {**kwargs, **attention}
+                att = cls.branch(encoder_outputs[::-1], name='attention', **attention_args)
+
+            # Decoder: transition up
+            if decoder is not None:
+                decoder_args = {**kwargs, **decoder}
+                main = cls.branch([emb] + att[:-1], name='decoder', **decoder_args)
+
+        return main[-1], att[-1], raw, offset
